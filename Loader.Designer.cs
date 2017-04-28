9c572eefb80fb832594518edd54f8761e56e92fc
mvxxx <yohama8832@o2.pl>
2017-04-28 21:21:18 +0200

    design

--- Loader.Designer.cs
+++ Loader.Designer.cs
@@ -28,53 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dimX = new System.Windows.Forms.TextBox();
-            this.dimY = new System.Windows.Forms.TextBox();
-            this.unitWorldSizeY = new System.Windows.Forms.TextBox();
-            this.unitWorldSizeX = new System.Windows.Forms.TextBox();
             this.mainLabel = new System.Windows.Forms.Label();
             this.authorInfo = new System.Windows.Forms.Label();
-            this.CellDimLabel = new System.Windows.Forms.Label();
-            this.UnitWorldSizeLabel = new System.Windows.Forms.Label();
             this.authorPage = new System.Windows.Forms.LinkLabel();
             this.runButton = new System.Windows.Forms.Button();
             this.rotateBox = new System.Windows.Forms.CheckBox();
             this.helpButton = new System.Windows.Forms.LinkLabel();
+            this.sourceButton = new System.Windows.Forms.Button();
+            this.pathTB = new System.Windows.Forms.TextBox();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.layerTB = new System.Windows.Forms.TextBox();
+            this.data = new System.Windows.Forms.ComboBox();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.layerLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // dimX
-            // 
-            this.dimX.Location = new System.Drawing.Point(150, 67);
-            this.dimX.Name = "dimX";
-            this.dimX.Size = new System.Drawing.Size(100, 20);
-            this.dimX.TabIndex = 0;
-            // 
-            // dimY
-            // 
-            this.dimY.Location = new System.Drawing.Point(150, 93);
-            this.dimY.Name = "dimY";
-            this.dimY.Size = new System.Drawing.Size(100, 20);
-            this.dimY.TabIndex = 1;
-            // 
-            // unitWorldSizeY
-            // 
-            this.unitWorldSizeY.Location = new System.Drawing.Point(150, 164);
-            this.unitWorldSizeY.Name = "unitWorldSizeY";
-            this.unitWorldSizeY.Size = new System.Drawing.Size(100, 20);
-            this.unitWorldSizeY.TabIndex = 3;
-            // 
-            // unitWorldSizeX
-            // 
-            this.unitWorldSizeX.Location = new System.Drawing.Point(150, 138);
-            this.unitWorldSizeX.Name = "unitWorldSizeX";
-            this.unitWorldSizeX.Size = new System.Drawing.Size(100, 20);
-            this.unitWorldSizeX.TabIndex = 2;
             // 
             // mainLabel
             // 
             this.mainLabel.AutoSize = true;
             this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.mainLabel.Location = new System.Drawing.Point(25, -2);
+            this.mainLabel.Location = new System.Drawing.Point(98, -2);
             this.mainLabel.Name = "mainLabel";
             this.mainLabel.Size = new System.Drawing.Size(225, 31);
             this.mainLabel.TabIndex = 4;
@@ -84,38 +58,16 @@
             // 
             this.authorInfo.AutoSize = true;
             this.authorInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.authorInfo.Location = new System.Drawing.Point(100, 29);
+            this.authorInfo.Location = new System.Drawing.Point(170, 29);
             this.authorInfo.Name = "authorInfo";
             this.authorInfo.Size = new System.Drawing.Size(78, 20);
             this.authorInfo.TabIndex = 5;
             this.authorInfo.Text = "by mvxxx";
             // 
-            // CellDimLabel
-            // 
-            this.CellDimLabel.AutoSize = true;
-            this.CellDimLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.CellDimLabel.Location = new System.Drawing.Point(28, 67);
-            this.CellDimLabel.Name = "CellDimLabel";
-            this.CellDimLabel.Size = new System.Drawing.Size(114, 48);
-            this.CellDimLabel.TabIndex = 6;
-            this.CellDimLabel.Text = "                                x:\r\nCell dimensions:\r\n                           " +
-    "     y:\r\n";
-            // 
-            // UnitWorldSizeLabel
-            // 
-            this.UnitWorldSizeLabel.AutoSize = true;
-            this.UnitWorldSizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.UnitWorldSizeLabel.Location = new System.Drawing.Point(28, 136);
-            this.UnitWorldSizeLabel.Name = "UnitWorldSizeLabel";
-            this.UnitWorldSizeLabel.Size = new System.Drawing.Size(114, 48);
-            this.UnitWorldSizeLabel.TabIndex = 7;
-            this.UnitWorldSizeLabel.Text = "                                x:\r\nAmmount of cells:\r\n                          " +
-    "      y:\r\n";
-            // 
             // authorPage
             // 
             this.authorPage.AutoSize = true;
-            this.authorPage.Location = new System.Drawing.Point(12, 240);
+            this.authorPage.Location = new System.Drawing.Point(28, 195);
             this.authorPage.Name = "authorPage";
             this.authorPage.Size = new System.Drawing.Size(90, 13);
             this.authorPage.TabIndex = 8;
@@ -125,7 +77,7 @@
             // 
             // runButton
             // 
-            this.runButton.Location = new System.Drawing.Point(197, 232);
+            this.runButton.Location = new System.Drawing.Point(356, 185);
             this.runButton.Name = "runButton";
             this.runButton.Size = new System.Drawing.Size(75, 23);
             this.runButton.TabIndex = 10;
@@ -136,7 +88,7 @@
             // rotateBox
             // 
             this.rotateBox.AutoSize = true;
-            this.rotateBox.Location = new System.Drawing.Point(151, 199);
+            this.rotateBox.Location = new System.Drawing.Point(236, 189);
             this.rotateBox.Name = "rotateBox";
             this.rotateBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.rotateBox.Size = new System.Drawing.Size(99, 17);
@@ -147,7 +99,7 @@
             // helpButton
             // 
             this.helpButton.AutoSize = true;
-            this.helpButton.Location = new System.Drawing.Point(122, 240);
+            this.helpButton.Location = new System.Drawing.Point(124, 195);
             this.helpButton.Name = "helpButton";
             this.helpButton.Size = new System.Drawing.Size(29, 13);
             this.helpButton.TabIndex = 12;
@@ -155,23 +107,94 @@
             this.helpButton.Text = "Help";
             this.helpButton.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.helpButton_LinkClicked);
             // 
+            // sourceButton
+            // 
+            this.sourceButton.Location = new System.Drawing.Point(356, 81);
+            this.sourceButton.Name = "sourceButton";
+            this.sourceButton.Size = new System.Drawing.Size(75, 23);
+            this.sourceButton.TabIndex = 13;
+            this.sourceButton.Text = "Add  source";
+            this.sourceButton.UseVisualStyleBackColor = true;
+            this.sourceButton.Click += new System.EventHandler(this.sourceButton_Click);
+            // 
+            // pathTB
+            // 
+            this.pathTB.Location = new System.Drawing.Point(18, 83);
+            this.pathTB.Name = "pathTB";
+            this.pathTB.Size = new System.Drawing.Size(100, 20);
+            this.pathTB.TabIndex = 14;
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(127, 84);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(100, 20);
+            this.nameTB.TabIndex = 15;
+            // 
+            // layerTB
+            // 
+            this.layerTB.Location = new System.Drawing.Point(238, 84);
+            this.layerTB.Name = "layerTB";
+            this.layerTB.Size = new System.Drawing.Size(100, 20);
+            this.layerTB.TabIndex = 16;
+            // 
+            // data
+            // 
+            this.data.FormattingEnabled = true;
+            this.data.Location = new System.Drawing.Point(12, 160);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(121, 21);
+            this.data.TabIndex = 17;
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pathLabel.Location = new System.Drawing.Point(44, 65);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(36, 15);
+            this.pathLabel.TabIndex = 18;
+            this.pathLabel.Text = "Path";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.Location = new System.Drawing.Point(157, 65);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(45, 15);
+            this.nameLabel.TabIndex = 19;
+            this.nameLabel.Text = "Name";
+            // 
+            // layerLabel
+            // 
+            this.layerLabel.AutoSize = true;
+            this.layerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.layerLabel.Location = new System.Drawing.Point(235, 65);
+            this.layerLabel.Name = "layerLabel";
+            this.layerLabel.Size = new System.Drawing.Size(105, 15);
+            this.layerLabel.TabIndex = 20;
+            this.layerLabel.Text = "Layer (number)";
+            // 
             // Loader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(437, 212);
+            this.Controls.Add(this.layerLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.layerTB);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.pathTB);
+            this.Controls.Add(this.sourceButton);
             this.Controls.Add(this.helpButton);
             this.Controls.Add(this.rotateBox);
             this.Controls.Add(this.runButton);
             this.Controls.Add(this.authorPage);
-            this.Controls.Add(this.UnitWorldSizeLabel);
-            this.Controls.Add(this.CellDimLabel);
             this.Controls.Add(this.authorInfo);
             this.Controls.Add(this.mainLabel);
-            this.Controls.Add(this.unitWorldSizeY);
-            this.Controls.Add(this.unitWorldSizeX);
-            this.Controls.Add(this.dimY);
-            this.Controls.Add(this.dimX);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Loader";
             this.ShowIcon = false;
@@ -182,19 +205,20 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox dimX;
-        private System.Windows.Forms.TextBox dimY;
-        private System.Windows.Forms.TextBox unitWorldSizeY;
-        private System.Windows.Forms.TextBox unitWorldSizeX;
         private System.Windows.Forms.Label mainLabel;
         private System.Windows.Forms.Label authorInfo;
-        private System.Windows.Forms.Label CellDimLabel;
-        private System.Windows.Forms.Label UnitWorldSizeLabel;
         private System.Windows.Forms.LinkLabel authorPage;
         private System.Windows.Forms.Button runButton;
         private System.Windows.Forms.CheckBox rotateBox;
         private System.Windows.Forms.LinkLabel helpButton;
+        private System.Windows.Forms.Button sourceButton;
+        private System.Windows.Forms.TextBox pathTB;
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.TextBox layerTB;
+        private System.Windows.Forms.ComboBox data;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label layerLabel;
     }
 }
 
